4a52b82597a94bceb4bc4a56bb66aca31f034534
Samuel Stephen <samuelstephen1234@gmail.com>
2025-09-06 21:31:55 +0530

    Add functionality to save to useLocalStorage.js

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -4,6 +4,9 @@ function useLocalStorage(key, initialValue) {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialValue;
   });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, setValue]);
   return [value, setValue];
 }
 export default useLocalStorage;
